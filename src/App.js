9d4d5662370723e8b2480be1d6ffd59c5bfdad78
Yasir Fayyaz <yasirfayyaz047@gmail.com>
2021-09-20 13:14:38 +0500

    second change

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  const title = "welcome to the New Blog"
+  const likes = 50
+  return (
+    <div className="App">
+      <div className="content">
+        <h1>{title}</h1>
+        <p>Liked {likes} times</p>
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
